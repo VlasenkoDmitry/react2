35cbea73f068ce147396a2d3f02da3aa8bd391f7
dzmitryUlasenka <dzmitry.ulasenka@innowise.com>
2024-10-29 17:47:23 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,62 @@ import { useState, useEffect } from "react";
 
 import { UserInfoInterface } from "./Interfaces/UserInfoInterface";
 
-function App() {
+import {MyComponent} from './123'
+
+function App() {  
+
+// type GenericIdentityFn = <UserInfoInterface[]>(UserInfoInterface[]) => ([UserInfoInterface[],(UserInfoInterface[])=>void])
+
+
+// function identity<Type>(arg: Type): Type {
+//   return arg;
+// }
+ 
+// let myIdentity: GenericIdentityFn = identity;
+
+// // Type '{ <S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>]; <S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>]; }' must have a '[Symbol.iterator]()' method that returns an iterator.ts(2488)
+
+
+//   // const myState: <UserInfoInterface>(UserInfoInterface[])
+//   const myState1 : <UserInfoInterface[]>(initialState: UserInfoInterface[] | (() => UserInfoInterface[])): [UserInfoInterface[], React.Dispatch<React.SetStateAction<UserInfoInterface[]>>] = useState
+//   const myState: (UserInfoInterface[]) => ([UserInfoInterface[],(UserInfoInterface[])=>void]) = useState
+
+//   let myIdentity: <UserInfoInterface[]>(UserInfoInterface[]) => [UserInfoInterface[],(UserInfoInterface[])=>void] = useState
+
+
+
+//   function identity<Type>(arg: Type): Type {
+//     return arg;
+//   }
+
+  // let myIdentity1: <Input>(arg: Input) => Input = identity;
+
+
+
+function identity<Type>(arg: Type): Type {
+  return arg;
+}
+
+async function getDataFromServer<Type>(arg:string) : Promise<Type[]> {
+  const responce = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  const jsonResponce: Type[] = await responce.json();
+  return jsonResponce
+}
+
+
   const [userInfo, setUserInfo] = useState<UserInfoInterface[]>([]);
 
   useEffect(() => {
     async function getData() {
       try {
-        const responce = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        const jsonResponce: UserInfoInterface[] = await responce.json();
-        setUserInfo(jsonResponce);
+        // const responce = await fetch(
+        //   "https://jsonplaceholder.typicode.com/users"
+        // );
+        // const jsonResponce: UserInfoInterface[] = await responce.json();
+        const promise = await getDataFromServer<UserInfoInterface>('https://jsonplaceholder.typicode.com/users')
+        setUserInfo(promise);
       } catch (error) {
         console.log(error);
       }
@@ -35,6 +80,7 @@ function App() {
           <div className="main-block">
             <h3>Hello Evano 👋🏼,</h3>
             <Table usersInfo={userInfo} />
+            <MyComponent />
           </div>
         </div>
       </main>
